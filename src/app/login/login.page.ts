f7b1d1c049dc6c9cde905cfe0abc10dfe329f126
AnderSon277 <anderson.cordova@epn.edu.ec>
2022-05-27 18:53:45 -0500

    conect with firebase

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +10,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+credentials:FormGroup  
+  constructor( 
+    private fb: FormBuilder,
+    private loadingController:LoadingController,
+    private alertController: AlertController,
+    private authService: AuthService,
+    private router: Router ) {}
 
-  ngOnInit() {}
+  get email(){
+    return this.credentials.get('email')
+  }
+
+  ngOnInit() {
+    this.credentials=this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.register(this.credentials.value);
+    await loading.dismiss();
+
+    if(user){
+      this.router.navigateByUrl('/home', {replaceUrl:true});
+    }else[
+      this.showAlert("Login failed", "try again")
+    ]
+  }
+
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.login(this.credentials.value,0);
+    await loading.dismiss();
+
+    if(user){
+      this.router.navigateByUrl('/home', {replaceUrl:true});
+    }else[
+      this.showAlert("Login failed", "try again")
+    ]
+
+  }
+
+  async showAlert(header,message){
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
